728346279ca086721b144763ca9d04953ebfa910
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-10 19:59:34 -0600

    finished project 2

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 
 
@@ -17,11 +17,20 @@ router.get('/', (req, res) => {
         ],
         order:[['created_at', 'DESC']],
         include: [
-            {
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
               model: User,
               attributes: ['username']
             }
-          ]
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -43,6 +52,15 @@ router.get('/', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
       include: [
+        // include the Comment model here:
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
