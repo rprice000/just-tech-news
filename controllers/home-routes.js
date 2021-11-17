4cdc0bccf231fb7f9c9bfedb110c4cf459074c17
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-17 12:21:35 -0600

    finsihed feature/add-authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,4 +38,17 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+    
+      res.render('login');
+  });
+
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
 module.exports = router;
